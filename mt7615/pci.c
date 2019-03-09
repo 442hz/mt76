7d00c5a3871951b9b8be3a7d7932778d505cad41
Lorenzo Bianconi <lorenzo@kernel.org>
2019-03-09 17:59:10 +0100

    mt7615: rely on mt76_dma_tx_queue_skb
    Rely on mt76_dma_tx_queue_skb routine and move mt7615_tx_prepare_txp
    code in mt7615_tx_prepare_skb
    
    Signed-off-by: Lorenzo Bianconi <lorenzo@kernel.org>

--- mt7615/pci.c
+++ mt7615/pci.c
@@ -70,8 +70,8 @@ static int mt7615_pci_probe(struct pci_dev *pdev,
 	static const struct mt76_driver_ops drv_ops = {
 		/* txwi_size = txd size + txp size */
 		.txwi_size = MT_TXD_SIZE + sizeof(struct mt7615_txp),
-		.tx_prepare_txp = mt7615_tx_prepare_txp,
 		.tx_prepare_skb = mt7615_tx_prepare_skb,
+		.tx_map = mt76_dma_tx_map,
 		.rx_skb = mt7615_queue_rx_skb,
 		.rx_poll_complete = mt7615_rx_poll_complete,
 		.sta_ps = mt7615_sta_ps,
